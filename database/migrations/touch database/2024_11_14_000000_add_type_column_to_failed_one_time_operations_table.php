d00f7255542e1990f0fa08d949bab77aa1eaa91f
Ahmed-Abdelhafez98 <ahmed.emad.abdelhafez@gmail.com>
2024-11-14 15:43:05 +0200

    add alter to add type to the failed table

---
+++ database/migrations/touch database/2024_11_14_000000_add_type_column_to_failed_one_time_operations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperationManager;
+
+class AddTypeColumnToFailedOperationsTable extends Migration
+{
+    protected string $name;
+
+    public function __construct()
+    {
+        $this->name = 'failed_' . OneTimeOperationManager::getTableName();
+    }
+
+    public function up(): void
+    {
+        Schema::table($this->name, function (Blueprint $table) {
+            $table->string('type')->nullable()->after('name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table($this->name, function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
